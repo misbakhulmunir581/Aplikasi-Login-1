130fd23c8ca1b7576ed809ba1094cec0a97462a7
misbakhulmunir581 <56871810+misbakhulmunir581@users.noreply.github.com>
2022-05-08 22:16:23 +0700

    Update login-script.js
    mengganti komentar sesuai dengan formatjsDoc (revisi)

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,33 +1,33 @@
-/** @module Login-Script */
-/** 
+/* @module Login-Script */
+/* 
 * Membuat variabel loginFormElement untuk tampilan form.
 * @constant {HTMLElement}
 */
 const loginFormElement = document.querySelector('#loginForm');
  
  
-/** 
+/* 
 * Membuat variabel inputEmailElement untuk tampilan input email.
 * @constant {HTMLElement}
 */
 const inputEmailElement = document.querySelector('#inputEmail');
  
  
-/** 
+/* 
 * Membuat variabel inputPasswordElement untuk tampilan input password.
 * @constant {HTMLElement}
 */
 const inputPasswordElement = document.querySelector('#inputPassword');
  
  
-/** 
+/* 
  * Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
  * @constant {string}
  */
 const expectedEmail = 'admin@dicoding.com';
  
  
-/** 
+/* 
  * Membuat variabel expectedPassword untuk menyimpan informasi password sementara.
  * @constant {string}
  */
@@ -38,13 +38,13 @@ const expectedPassword = 'superpassword'
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
   
-  /** 
+  /* 
    * Membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan.
    * @constant {string}
    */
   const email = inputEmailElement.value;
   
-  /** 
+  /* 
    * Membuat variabel password untuk menyimpan niali password yang didapatkan saat button ditekan.
    * @constant {string}
    */
